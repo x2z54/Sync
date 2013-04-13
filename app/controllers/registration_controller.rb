f76ec13f7ea39d1feaeb1bd36dc38bab50eecd18
x2z54 <flashbulb54@gmail.com>
2013-04-13 11:05:42 +0400

    download

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -20,7 +20,7 @@ class RegistrationController < ApplicationController
 	end
 
 	def download
-		send_file "/public/dist.zip", :type=>"application/zip"
+		send_file "/public/sync.py.zip", :type=>"application/zip"
 	end
 
 	def refreshDB
